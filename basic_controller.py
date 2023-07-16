ac875f670e177721536ad200d3efc715ebd117a6
Anshu Madhikarmi <madhikarmianshu@gmail.com>
2023-07-17 09:29:32 +1000

    Update basic_controller.py

--- basic_controller.py
+++ basic_controller.py
@@ -6,20 +6,14 @@ class OController(Topo):
 
     def build(self):
 
-        # Add the OpenFlow reference controller
-        controller = self.addController('c0')
-
         # Add the central switch
         s1 = self.addSwitch('s1')
 
-        # Connect two hosts to the switch
-        h1 = self.addHost("h1")
-        h2 = self.addHost("h2")
-        self.addLink(s1, h1, cls=TCLink, bw=40, delay='15ms')
-        self.addLink(s1, h2, cls=TCLink, bw=40, delay='15ms')
-
-        # Set the controller for the switch
-        self.addController(s1, controller)
+        # connect n hosts to the switch
+        hosts = []
+        for h in range(0, 5):
+            hosts.append(self.addHost("h{}".format(h+1)))
+            self.addLink(s1, hosts[h], cls=TCLink, bw=40, delay='15ms')
 
 
 # the topology accessible to the mn tool's `--topo` flag
